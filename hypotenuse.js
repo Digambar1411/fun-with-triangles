b87c404d750c35b7eb286e76528e36abb25fc8e8
Digambar <deshawaldigambar@gmail.com>
2021-09-19 23:41:07 +0530

    Create hypotenuse.js

---
+++ hypotenuse.js
@@ -0,0 +1,11 @@
+var side1=document.querySelector("#side1");
+var side2=document.querySelector("#side2");
+var calculateBtn=document.querySelector("#btn");
+var output=document.querySelector("#output");
+console.log(side1);
+console.log(side2);
+output.innerText="output will show here";
+
+
+
+
