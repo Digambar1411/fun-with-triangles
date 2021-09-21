905d0e8facdd85c7ba5d0c57ad34a6a210287338
Digambar <deshawaldigambar@gmail.com>
2021-09-21 16:47:35 +0530

    feat: added error message

--- hypotenuse.js
+++ hypotenuse.js
@@ -1,18 +1,40 @@
 var firstSide=document.querySelector("#side1");
 var secondSide=document.querySelector("#side2");
 var calculateBtn=document.querySelector("#btn");
+var errorMsg=document.querySelector("#err-msg");
 var result=document.querySelector("#output");
 
 function calculateHypotenuse(){
+    if(firstSide.value && secondSide.value){
+        hideMessage();
+        var a=Number(firstSide.value);
+        var b=Number(secondSide.value);
+        var hypotenuseOfTriangle= Math.sqrt(a*a+b*b).toFixed(3);
+        result.style.display="block";
+        result.innerText=" Hypotenuse of the trianglr is "+hypotenuseOfTriangle+" units";
     
-    var a=Number(firstSide.value);
-    var b=Number(secondSide.value);
-    var hypotenuseOfTriangle= Math.sqrt(a*a+b*b).toFixed(3);
-    result.innerText=" Hypotenuse Of the triangle is "+hypotenuseOfTriangle+" units";
+    }
+    else{
+        showMessage("Note: You have to fill all the fields and values in them must be greate than zero");
+    }
 
       
 }
 calculateBtn.addEventListener("click", calculateHypotenuse);
+
+function hideMessage(){
+    errorMsg.style.display="none";
+    result.style.display="none";
+
+}
+
+function showMessage(msg){
+    errorMsg.style.display="block";
+    errorMsg.innerText=msg;
+    
+    
+    
+}
 
 
 
