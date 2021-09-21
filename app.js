d59d9ba036b7b54af670e857bd3fabec3829a1a0
Digambar <deshawaldigambar@gmail.com>
2021-09-21 10:02:11 +0530

    all html part completes

--- app.js
+++ app.js
@@ -9,7 +9,21 @@ function calculateSumOfAngles(angle1,angle2,angle3){
     return Number(angle1)+Number(angle2)+Number(angle3)
 }
 
-console.log(calculateSumOfAngles(23,45,80));
-console.log(calculateSumOfAngles(65,45,70));
+function isTriangle(){
+    if(angle1.value&&angle2.value&&angle3.value){
+        var sumOfAngles=calculateSumOfAngles(angle1.value, angle2.value, angle3.value);
+        if(sumOfAngles===180){
+            output.innerText="oh YES Angles form a Triangle";
+        }
+        else{
+            output.innerText="oh NO Angles won't form a Triangle";
+        }
+    }
+    else{
+        alert("Please fill all the values");
+    }
 
+}
+
+isTriangleBtn.addEventListener("click", isTriangle);
 
