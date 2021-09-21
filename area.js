0dce4f7f39ba2440bf6bf876623877533577a7ee
Digambar <deshawaldigambar@gmail.com>
2021-09-21 12:11:14 +0530

    input output processing

--- area.js
+++ area.js
@@ -2,12 +2,28 @@ var base= document.querySelector("#base");
 var height= document.querySelector("#height");
 var errorMsg= document.querySelector("#err-msg");
 var areaBtn= document.querySelector("#area-btn");
-var output= document.querySelector("#output");
+var output=document.querySelector("#output");
 
-console.log(base);
-console.log(height);
-console.log(errorMsg);
+// console.log(base);
+// console.log(height);
+// console.log(errorMsg);
+// console.log(output);
 
-areaBtn.addEventListener("click", ()=> console.log("clicke"));
-errorMsg.innerText="thhis is error msg";
-output.innerText="output wil show here";
+areaBtn.addEventListener("click", calculateArea);
+
+function calculateArea(){
+    
+    if(base.value && height.value){
+        var triangleBase=Number(base.value);
+        var triangleHeight=Number(height.value);
+        var areaOfTriangle=(triangleBase*triangleHeight)/2;
+        output.innerText="Area of the triangle is "+areaOfTriangle +" units";
+
+    }
+    
+
+}
+
+
+
+
