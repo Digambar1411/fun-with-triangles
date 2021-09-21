f40ed06462cd30c69c90fda97171753b13b7e5a7
Digambar <deshawaldigambar@gmail.com>
2021-09-21 22:57:04 +0530

    accessed the form values using for loop

--- quiz.js
+++ quiz.js
@@ -1,13 +1,25 @@
 var quizForm= document.querySelector("#quiz-form");
 var errorMsg= document.querySelector("#err-msg");
 var output= document.querySelector("#out-div");
-var submitBtn=document.querySelector("submit-btn");
-
-submitBtn.addEventListener("click", checkScore);
-
-function checkScore(){
-
-}
+var submitBtn=document.querySelector("#submit-btn");
 
 
 var answers=["Yes","Yes","No","Yes","Isosceles","30","Yes","Both","5cm","Scalene"]
+
+function checkScore(){
+    var score=0;
+    var index=0;
+
+    var formResult = new FormData(quizForm);
+    for(var entry of formResult.entries()){
+        console.log(entry);
+
+    }
+    for( var value of formResult.values()){
+        console.log(value);
+    }
+}    
+
+submitBtn.addEventListener("click", checkScore);
+
+
