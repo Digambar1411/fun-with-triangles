8ee1773f155aae50a5346a74201ced6eea49fcd1
Digambar <deshawaldigambar@gmail.com>
2021-09-21 22:23:02 +0530

    selected elements using querySelector

---
+++ quiz.js
@@ -0,0 +1,13 @@
+var quizForm= document.querySelector("#quiz-form");
+var errorMsg= document.querySelector("#err-msg");
+var output= document.querySelector("#out-div");
+var submitBtn=document.querySelector("submit-btn");
+
+submitBtn.addEventListener("click", checkScore);
+
+function checkScore(){
+
+}
+
+
+var answers=["Yes","Yes","No","Yes","Isosceles","30","Yes","Both","5cm","Scalene"]
