93354fe1cc0bb2b14eaaabd3cc755f934be1de75
Digambar <deshawaldigambar@gmail.com>
2021-09-21 12:33:05 +0530

    feat: added error message
    to notifies user added error message

--- area.js
+++ area.js
@@ -1,27 +1,35 @@
 var base= document.querySelector("#base");
 var height= document.querySelector("#height");
-var errorMsg= document.querySelector("#err-msg");
 var areaBtn= document.querySelector("#area-btn");
 var output=document.querySelector("#output");
-
-// console.log(base);
-// console.log(height);
-// console.log(errorMsg);
-// console.log(output);
-
+var errorMsg=document.querySelector("#err-msg");
 areaBtn.addEventListener("click", calculateArea);
 
 function calculateArea(){
-    
+    hideMessage();
     if(base.value && height.value){
         var triangleBase=Number(base.value);
         var triangleHeight=Number(height.value);
         var areaOfTriangle=(triangleBase*triangleHeight)/2;
+        output.style.display="block";
         output.innerText="Area of the triangle is "+areaOfTriangle +" units";
 
     }
+    else{
+        showMessage("Note: You have to enter all the fields and values must be greater than zero!");
+    }
     
 
+}
+
+function hideMessage(){
+    errorMsg.style.display="none";
+    output.style.display="none";
+
+}
+function showMessage(msg){
+    errorMsg.innerText=msg;
+    errorMsg.style.display="block";
 }
 
 
