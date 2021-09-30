b902c5006c3764e4558e006e641163d3d29eea84
Digambar <deshawaldigambar@gmail.com>
2021-09-30 20:49:18 +0530

    fix: result message
    fixed error of warning and result message

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var angle1 = document.querySelector("#angle1");
 var angle2 = document.querySelector("#angle2");
 var angle3 = document.querySelector("#angle3");
 var isTriangleBtn=document.querySelector("#btn");
+var errorMsg=document.querySelector("#err-msg");
 var output=document.querySelector("#output");
 
 
@@ -10,18 +11,31 @@ function calculateSumOfAngles(angle1,angle2,angle3){
 }
 
 function isTriangle(){
-    if(angle1.value&&angle2.value&&angle3.value){
-        var sumOfAngles=calculateSumOfAngles(angle1.value, angle2.value, angle3.value);
-        if(sumOfAngles===180){
-            output.innerText="oh YES Angles form a Triangle";
+    errorMsg.style.display="none";
+    output.style.display="none";
+    if(angle1.value && angle2.value  && angle3.value){
+        if(angle1.value !=0 && angle2.value !=0 && angle3.value!=0){
+    
+            var sumOfAngles=calculateSumOfAngles(angle1.value, angle2.value, angle3.value);
+            if(sumOfAngles===180){
+                output.style.display="block";
+                output.innerText="Oh YES !! Angles form a Triangle";
+            }
+            else{
+                output.style.display="block";
+                output.innerText="Oh NO!! Angles won't form a Triangle";
+            }
         }
         else{
-            output.innerText="oh NO Angles won't form a Triangle";
+            errorMsg.style.display="block";
+            errorMsg.innerText="Alert : Angle entered must be greater than zero";
         }
-    }
+    }    
     else{
-        alert("Please fill all the values");
-    }
+        errorMsg.style.display="block";
+        errorMsg.innerText="Alert : You must enter all the values";
+
+    }    
 
 }
 
